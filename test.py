4c4b4ee7b0c3fee2b0b180f5bab89dd4067ed187
ghosthacker239 <77110826+ghosthacker239@users.noreply.github.com>
2023-04-17 01:03:48 +0900

    Add files via upload

--- test.py
+++ test.py
@@ -1,69 +1,22 @@
 import requests
-import json
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-from bs4 import BeautifulSoup
+import time
 
-# Send GET request to webpage and retrieve HTML content
-url = 'https://coinmarketcap.com/currencies/dogecoin/?period=7d'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+URL = "https://coinmarketcap.com/currencies/dogecoin/?period=7d"
+MASK = "..........."
+HEX = ['1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
-# Extract chart data
-chart_data = soup.find('script', id='__NEXT_DATA__').string
-chart_data = chart_data.replace('window.__INITIAL_STATE__=', '').replace(';', '')
-chart_data = json.loads(chart_data)
-chart_data = chart_data['cryptocurrency']['ohlcvHistorical'][::-1]
+TOKEN = ""
+count = 0;
+check = True
 
-# Convert chart data to Pandas dataframe
-df = pd.DataFrame(chart_data, columns=['time', 'open', 'high', 'low', 'close', 'volume', 'market_cap'])
-df['time'] = pd.to_datetime(df['time'], unit='ms')
+time_start = time.time()
 
-# Calculate moving average
-ma = df['close'].rolling(window=20).mean()
+while (check):
+    responB = int(requests.get(URL+MASK+MASK).headers.get('Content-Length'))
+    
+    for iter in HEX:
+        count+=1
+        build1 = TOKEN+iter+MASK+MASK
+        build2 = TOKEN+MASK+iter+MASK
 
-# Plot moving average and candlestick chart
-fig, ax = plt.subplots()
-ax.plot(df['time'], df['close'], label='Dogecoin')
-ax.plot(df['time'], ma, label='Moving Average')
-ax.legend()
-ax.set_xlabel('Time')
-ax.set_ylabel('Price')
-ax.set_title('Dogecoin Price Chart')
-
-# Fit linear regression model to predict future trends
-model = LinearRegression()
-X = df['time'].values.reshape(-1, 1)
-y = df['close'].values.reshape(-1, 1)
-model.fit(X, y)
-future_dates = pd.date_range(start='2023-04-17', end='2023-05-17')
-future_dates = future_dates.to_series()
-future_dates = future_dates.dt.strftime('%Y-%m-%d')
-future_dates = pd.to_datetime(future_dates)
-future_dates = future_dates.values.reshape(-1, 1)
-predictions = model.predict(future_dates)
-
-# Plot predicted trends
-fig, ax = plt.subplots()
-ax.plot(df['time'], df['close'], label='Dogecoin')
-ax.plot(future_dates, predictions, label='Predictions')
-ax.legend()
-ax.set_xlabel('Time')
-ax.set_ylabel('Price')
-ax.set_title('Dogecoin Price Predictions')
-
-# Estimate best time to buy based on predictions and current price
-current_price = df['close'].iloc[-1]
-best_time_to_buy = future_dates[predictions.argmin()]
-print("The current price of Dogecoin is ${:.2f}.".format(current_price))
-print("The estimated best time to buy Dogecoin is on {}.".format(best_time_to_buy.strftime('%Y-%m-%d')))
-import requests
-import json
-
-url = 'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/ohlcv/historical?id=74&convertId=2781&timeStart=1649536800000&timeEnd=1649882400000'
-
-response = requests.get(url)
-chart_data = json.loads(response.content)
-
-print(chart_data)
+        req1 = int(requests.get(URL+build1).headers.get('Content-Length'))
